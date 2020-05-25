2faf9f86b5f8a2fecfeaa60ccce464f45460bdb9
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-25 10:24:14 +0300

    New task has been added

---
+++ Stacks and Queues - Exercise/1. Basic Stack Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> elements = new Stack<int>();
+
+            int[] inputInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int elementsToPush = inputInfo[0];
+            int elementsToPop = inputInfo[1];
+            int targetNum = inputInfo[2];
+            bool elementFound = false;
+
+            int[] sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < elementsToPush; i++)
+            {
+                elements.Push(sequence[i]);
+            }
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                elements.Pop();
+            }
+
+            if (elements.Contains(targetNum))
+            {
+                elementFound = true;
+            }
+
+            if (elements.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (elementFound)
+            {
+                Console.WriteLine("true");
+            }
+            else if (!elementFound)
+            {
+                Console.WriteLine(elements.Min()); 
+            }
+        }
+    }
+}
