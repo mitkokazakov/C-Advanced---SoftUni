7f302e642565b590e36dc23987a6bc3383f0122d
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-26 19:51:26 +0300

    New task

---
+++ Stacks and Queues - Exercise/4.  Fast Food/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.__Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>(input);
+            bool isEnoughFood = true;
+
+            
+            int biggestOrder = 0;
+            int queueLen = orders.Count;
+
+            if (orders.Any())
+            {
+                Console.WriteLine(orders.Max());
+            }
+            
+
+
+            for (int i = 0; i < queueLen; i++)
+            {
+                int currentOrder = orders.Peek();
+
+                if (currentOrder <= quantityOfFood)
+                {
+                    if (currentOrder > biggestOrder)
+                    {
+                        biggestOrder = currentOrder;
+                    }
+                    quantityOfFood -= orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {String.Join(" ", orders)}");
+                    isEnoughFood = false;
+                }
+            }
+
+            
+                
+            
+            
+
+            if (orders.Count == 0 && isEnoughFood)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            
+        }
+    }
+}
