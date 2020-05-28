c5230a52913e54f89f95df69f4d1eb863de7f9d1
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-28 18:38:29 +0300

    New task

---
+++ Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+
+                if (currentChar == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (currentChar == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    string content = text.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(content);
+                }
+            }
+        }
+    }
+}
