73263ad26caf7e1d80de781acbdddf428d412760
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-30 19:50:27 +0300

    New task has been added

---
+++ Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> potato = new Queue<string>(names);
+
+            int counter = 0;
+
+            while (potato.Count > 1)
+            {
+                counter++;
+                if (counter != n)
+                {
+                    string kidGoesToEnd = potato.Dequeue();
+                    potato.Enqueue(kidGoesToEnd);
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {potato.Dequeue()}");
+                    counter = 0;
+                }
+           
+            }
+
+            Console.WriteLine($"Last is {potato.Dequeue()}");
+        }
+    }
+}
