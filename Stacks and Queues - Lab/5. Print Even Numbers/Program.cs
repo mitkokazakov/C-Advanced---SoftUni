7787421586746063127338e428e3ba1d8d8140e5
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-30 19:48:48 +0300

    New task

---
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> evenNums = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenNums.Enqueue(numbers[i]);
+                }
+            }
+
+            
+            Console.WriteLine(String.Join(", ", evenNums));
+        }
+    }
+}
