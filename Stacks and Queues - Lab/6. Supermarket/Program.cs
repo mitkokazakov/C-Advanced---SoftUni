d400dc1e5aa43c510fa928a46e30c49deb479928
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-30 19:49:55 +0300

    Another task has been added

---
+++ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<string> customers = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                    customers.Enqueue(input);
+                }
+                else if (input == "Paid")
+                {
+                    Console.WriteLine(String.Join(Environment.NewLine, customers));
+                    customers.Clear();
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{customers.Count} people remainng");
+        }
+    }
+}
