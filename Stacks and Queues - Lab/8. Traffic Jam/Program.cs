68d4b316e1ede792c991e116959153bfe718ebad
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-05-31 16:24:47 +0300

    Last task from the Lab

---
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            Queue<string> trafficJam = new Queue<string>();
+
+            int counterPassedCars = 0;
+
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    trafficJam.Enqueue(input);
+                }
+                else
+                {
+                    for (int i = 0; i < n && trafficJam.Any(); i++)
+                    {
+                        Console.WriteLine($"{trafficJam.Dequeue()} passed!");
+                        counterPassedCars++;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counterPassedCars} cars passed the crossroads.");
+        }
+    }
+}
