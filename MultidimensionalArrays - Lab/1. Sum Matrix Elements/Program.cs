b31ecab7016c0be1762fffe749507c72ca622de6
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-01 18:40:18 +0300

    New task has been added

---
+++ MultidimensionalArrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputColRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = inputColRow[0];
+            int cols = inputColRow[1];
+
+            int[,] matrix = new int[rows,cols];
+
+            //Fill the matrix with elements
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] singleRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = singleRow[col];
+                }
+            }
+
+            //Print the matrix
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        Console.Write(matrix[i,j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Sum all elements of the matrix
+            int sum = 0;
+
+            foreach (var elememt in matrix)
+            {
+                sum += elememt;
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
