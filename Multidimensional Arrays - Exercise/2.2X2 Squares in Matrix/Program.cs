a9b72045367f347d595c0621c6adfe539d670725
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-06 18:20:13 +0300

    New task

---
+++ Multidimensional Arrays - Exercise/2.2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Maximal_Sum
+{
+   C# Advanced Exam - 26 October 2019 class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = size[0];
+            int cols = size[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            int n = 2;
+
+            int counter = 0;
+
+            FillMatrix(rows, cols, matrix);
+
+            for (int row = 0; row <= rows - n; row++)
+            {
+                for (int col = 0; col <= cols - n; col++)
+                {
+
+                   
+                    if (IsAllEqual(matrix, row, col))
+                    {
+                        counter++;
+                    }
+
+
+
+
+                }
+            }
+
+            Console.WriteLine(counter);
+
+
+        }
+
+        private static void FillMatrix(int rows, int cols, char[,] matrix)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                char[] currentLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentLine[col];
+                }
+            }
+        }
+
+        private static bool IsAllEqual(char[,] matrix, int row, int col)
+        {
+            char currentElement = matrix[row, col];
+            return currentElement == matrix[row, col + 1] && currentElement == matrix[row + 1, col] && currentElement == matrix[row + 1, col + 1];
+        }
+    }
+}
+
