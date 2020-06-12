4b0f13f0a62b5267d9020ee7f1eef4fc2a9cb2a0
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-12 18:14:12 +0300

    New task

---
+++ SetsAndDictionariesAdvanced - Exercise/3. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLines = int.Parse(Console.ReadLine());
+            HashSet<string> periodicTable = new HashSet<string>();
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                string[] elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var element in elements)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", periodicTable.OrderBy(x => x)));
+        }
+    }
+}
