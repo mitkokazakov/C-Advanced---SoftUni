7cfd4c5dc95df36373f06b636d6697ccb982d06b
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-12 18:12:22 +0300

    New task added

---
+++ SetsAndDictionariesAdvanced - Exercise/1. Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOffLines = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < numOffLines; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine,names));
+        }
+    }
+}
