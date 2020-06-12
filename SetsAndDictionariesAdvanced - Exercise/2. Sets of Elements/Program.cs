9f3f4d18c2bbc3b0dbb808417f916d206928be9d
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-12 18:13:33 +0300

    New task

---
+++ SetsAndDictionariesAdvanced - Exercise/2. Sets of Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int lenOfFirstSet = size[0];
+            int lenOfSecondSet = size[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < lenOfFirstSet; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSet.Add(number);
+            }
+
+            for (int i = 0; i < lenOfSecondSet; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondSet.Add(number);
+            }
+
+            foreach (var num in firstSet)
+            {
+                foreach (var number in secondSet)
+                {
+                    if (num == number)
+                    {
+                        Console.Write(num + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
