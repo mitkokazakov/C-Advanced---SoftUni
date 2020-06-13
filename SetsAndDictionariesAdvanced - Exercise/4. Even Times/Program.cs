1c7b509dbcad2cfe748ad2d717c0ca54e12982e2
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-13 12:28:43 +0300

    New task has been added

---
+++ SetsAndDictionariesAdvanced - Exercise/4. Even Times/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLines = int.Parse(Console.ReadLine());
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!counts.ContainsKey(number))
+                {
+                    counts[number] = 0;
+                }
+
+                counts[number]++;
+            }
+
+            foreach (var (key,value) in counts)
+            {
+                if (value % 2 == 0)
+                {
+                    Console.WriteLine(key);
+                }
+            }
+        }
+    }
+}
