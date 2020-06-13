939a88ae23e7d47452e3e47009721045b1bcd255
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-13 12:29:36 +0300

    New task

---
+++ SetsAndDictionariesAdvanced - Exercise/5. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> countSymbols = new Dictionary<char, int>();
+
+            string text = Console.ReadLine();
+
+            foreach (var symbol in text)
+            {
+                if (!countSymbols.ContainsKey(symbol))
+                {
+                    countSymbols[symbol] = 0;
+                }
+                countSymbols[symbol]++;
+            }
+
+            foreach (var (symbol,count) in countSymbols.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{symbol}: {count} time/s");
+            }
+        }
+    }
+}
