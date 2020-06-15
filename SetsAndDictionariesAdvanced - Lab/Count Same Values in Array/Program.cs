1a463a4c28ab3a92f1105fe45271440d1c6cf3ab
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-15 18:10:06 +0300

    New task

---
+++ SetsAndDictionariesAdvanced - Lab/Count Same Values in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> countSameValues = new Dictionary<double, int>();
+
+            foreach (var number in input)
+            {
+                if (!countSameValues.ContainsKey(number))
+                {
+                    countSameValues[number] = 0;
+                }
+
+                countSameValues[number]++;
+            }
+
+            foreach (var (key,value) in countSameValues)
+            {
+                Console.WriteLine($"{key} - {value} times");
+            }
+        }
+    }
+}
