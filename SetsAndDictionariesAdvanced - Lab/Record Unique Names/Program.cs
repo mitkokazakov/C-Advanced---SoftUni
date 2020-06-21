d7c7e3ceaf8dec8f42b927eec6bb4758138899c1
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-21 20:11:04 +0300

    New task

---
+++ SetsAndDictionariesAdvanced - Lab/Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOFLInes = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < numOFLInes; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine,names));
+        }
+    }
+}
