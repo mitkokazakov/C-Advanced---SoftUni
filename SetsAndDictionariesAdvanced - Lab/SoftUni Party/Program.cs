e5c8cba553d33ab9de737be863bf420fb4d260e0
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-23 20:07:30 +0300

    Last task from the lab

---
+++ SetsAndDictionariesAdvanced - Lab/SoftUni Party/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> casualGuests = new HashSet<string>();
+            HashSet<string> VIP = new HashSet<string>();
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (Char.IsDigit(input[0]))
+                {
+                    VIP.Add(input);
+                }
+                else
+                {
+                    casualGuests.Add(input);
+                }
+               
+
+                input = Console.ReadLine();
+            }
+
+            string guests = Console.ReadLine();
+
+            while (guests != "END")
+            {
+                if (Char.IsDigit(guests[0]))
+                {
+                    VIP.Remove(guests);
+                }
+                else
+                {
+                    casualGuests.Remove(guests);
+                }
+                guests = Console.ReadLine();
+            }
+
+            Console.WriteLine(VIP.Count + casualGuests.Count);
+
+            if (VIP.Count > 0)
+            {
+                Console.WriteLine(String.Join(Environment.NewLine,VIP));
+            }
+            if (casualGuests.Count > 0)
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, casualGuests));
+            }
+        }
+    }
+}
