4c7c4d58e11f8e15794c8f52cc5c81f9c0dd8795
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-23 20:10:11 +0300

    First task from this exercise has been added

---
+++ Streams, Files and Directories - Exercise/1. Even Lines/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _1._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                int counter = 0;
+                string[] symbols = { "-", ",", ".", "!", "?" };
+
+                while (!reader.EndOfStream)
+                {
+                    string currentLine = reader.ReadLine();
+
+                    if (counter % 2 == 0)
+                    {
+                        currentLine = ReplaceSymbols(symbols, currentLine);
+                        string[] finalResult = currentLine.Split().Reverse().ToArray();
+                        Console.WriteLine(String.Join(" ",finalResult));
+                    }
+                    counter++;
+                }
+            }
+        }
+
+        private static string ReplaceSymbols(string[] symbols, string currentLine)
+        {
+            foreach (var symbol in currentLine)
+            {
+                string currentSymbol = symbol.ToString();
+                if (symbols.Contains(currentSymbol))
+                {
+                    currentLine = currentLine.Replace(currentSymbol, "@");
+                }
+            }
+
+            return currentLine;
+        }
+    }
+}
