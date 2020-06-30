c8fbeb1aaabb8f521ffa85127b1599577b8bd652
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-06-30 17:49:37 +0300

    New task

---
+++ Streams, Files and Directories - Exercise/6. Zip and Extract/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace _6._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"D:\TU Gabrovo\Copy", @"D:\TU Gabrovo\Zip\MyArchive.zip");
+            ZipFile.ExtractToDirectory(@"D:\TU Gabrovo\Zip\MyArchive.zip", @"D:\TU Gabrovo\Extract");
+        }
+    }
+}
