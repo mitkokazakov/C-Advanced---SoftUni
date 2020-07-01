a6eb6de6296497a0bd26e7d88621185ed80c8c54
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-01 18:51:21 +0300

    New task

---
+++ Functional Programming - Exercise/4.1 Find Evens or Odd/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4._1_Find_Evens_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string query = Console.ReadLine();
+            List<int> output = new List<int>();
+
+            Predicate<int> oddOrEven = null;
+
+            if (query == "even")
+            {
+                oddOrEven = new Predicate<int>(n => n % 2 == 0);
+            }
+            else
+            {
+                oddOrEven = new Predicate<int>(n => n % 2 != 0);
+            }
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                if (oddOrEven(i))
+                {
+                    output.Add(i);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ",output));
+        }
+    }
+}
