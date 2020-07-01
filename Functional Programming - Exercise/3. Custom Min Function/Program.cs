eb90d9c1ddbf1d06dfa8e34ca57c3830b447884c
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-01 18:50:40 +0300

    New task

---
+++ Functional Programming - Exercise/3. Custom Min Function/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            //Action<int[]> printArray = array =>
+            //{
+            //    Console.WriteLine(String.Join(Environment.NewLine,array));
+            //};
+
+            Func<int[], int> findMinNumber = arr =>
+             {
+                 int minValue = int.MaxValue;
+
+                 foreach (var num in arr)
+                 {
+                     if (num < minValue)
+                     {
+                         minValue = num;
+                     }
+                 }
+
+                 return minValue;
+             };
+
+            int minNum = findMinNumber(nums);
+            Console.WriteLine(minNum);
+            //printArray(nums);
+        }
+    }
+}
