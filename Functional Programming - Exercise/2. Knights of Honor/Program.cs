f43b35e91b7873003ee90ab8d09078b110f1a3e2
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-01 18:49:43 +0300

    New task has been added

---
+++ Functional Programming - Exercise/2. Knights of Honor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> knights = name =>
+            {
+                Console.WriteLine("Sir " + name);
+            };
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(knights);
+        }
+    }
+}
