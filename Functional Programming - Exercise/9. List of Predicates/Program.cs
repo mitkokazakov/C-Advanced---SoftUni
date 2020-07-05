6f2d2ba66a4ec0fabc4546efc5be0ef3b2cfd488
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-05 12:41:58 +0300

    New task

---
+++ Functional Programming - Exercise/9. List of Predicates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _9._List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int endOffRange = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> outpuutNums = new List<int>();
+
+            bool isDivisible = true;
+
+            for (int i = 1; i <= endOffRange; i++)
+            {
+                int currentNum = i;
+
+                foreach (var divider in dividers)
+                {
+                    Predicate<int> division = n => currentNum % n == 0;
+
+                    if (division(divider))
+                    {
+                        isDivisible = true;
+                    }
+                    else
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible == true)
+                {
+                    outpuutNums.Add(currentNum);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ",outpuutNums));
+        }
+    }
+}
