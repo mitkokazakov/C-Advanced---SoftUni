db9b450184ed345287252e529c9f2570fb48d0aa
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-05 12:40:41 +0300

    Another task has been added

---
+++ Functional Programming - Exercise/6. Reverse And Exclude/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _6._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int numToDivide = int.Parse(Console.ReadLine());
+            List<int> outputNums = new List<int>();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                Predicate<int> predicate = n => n % numToDivide != 0;
+
+                if (predicate(nums[i]))
+                {
+                    outputNums.Add(nums[i]);
+                }
+            }
+
+            outputNums.Reverse();
+            Console.WriteLine(String.Join(" ",outputNums));
+        }
+    }
+}
