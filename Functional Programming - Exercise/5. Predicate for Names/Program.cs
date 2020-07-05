de449fb4dcaa97c90f28077eea5cdc2419150c3d
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-05 12:39:54 +0300

    New task

---
+++ Functional Programming - Exercise/5. Predicate for Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLen = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Predicate<string> lessOrEqualThanLen = new Predicate<string>(name => name.Length <= maxLen);
+
+            foreach (var name in names)
+            {
+                if (lessOrEqualThanLen(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
