65827e13f30e71c8170f772ac07d036e2784b2fe
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-09 22:34:37 +0300

    Last task from this exercise

---
+++ Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> succesNames = new List<string>();
+
+            Func<string, int, bool> IsSumLargerOrEqual = (name, sum) =>
+              {
+                  int currentSum = 0;
+
+                  foreach (var letter in name)
+                  {
+                      currentSum += letter;
+                  }
+
+                  return currentSum >= sum;
+              };
+
+            foreach (var name in names)
+            {
+                if (IsSumLargerOrEqual(name,n))
+                {
+                    succesNames.Add(name);
+                }
+            }
+
+            Console.WriteLine(succesNames.First());
+        }
+    }
+}
