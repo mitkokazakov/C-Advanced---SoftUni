5f41392a5484ee88d5092461cdc44757b72ebe1e
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-11 09:41:55 +0300

    New task

---
+++ Generics - Lab/2. Generic Array Creator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+
+            Console.WriteLine(String.Join("--",strings));
+        }
+    }
+}
