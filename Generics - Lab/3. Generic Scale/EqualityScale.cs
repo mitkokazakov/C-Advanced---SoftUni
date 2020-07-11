64bc995e88a4638df4c4374740bf6555ac3f204e
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-11 09:42:35 +0300

    New task

---
+++ Generics - Lab/3. Generic Scale/EqualityScale.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+      
+        public T Left
+        {
+            get
+            {
+                return this.left;
+            }
+            set
+            {
+                this.left = value;
+            }
+        }
+
+        
+
+        public T Right
+        {
+            get
+            {
+                return this.right;
+            }
+            set
+            {
+                this.right = value;
+            }
+        }
+
+        public bool AreEqual()
+        {
+            bool result = this.Right.Equals(this.Left);
+
+            return result;
+        }
+
+    }
+}
---
+++ Generics - Lab/3. Generic Scale/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int left = 5;
+            int right = 10;
+
+            EqualityScale<int> scale = new EqualityScale<int>(left, right);
+
+            var result = scale.AreEqual();
+
+            Console.WriteLine(result);
+        }
+    }
+}
