79bd104e196c180ba441e3329357e89d4d5b893e
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-11 09:40:44 +0300

    New task has been

---
+++ Generics - Lab/1. Box of T/Box.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Data = new Stack<T>();
+        }
+
+        public Stack<T> Data { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return this.Data.Count;
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.Data.Push(item);
+        }
+
+        public T Remove()
+        {
+            T removedElement = this.Data.Pop();
+            return removedElement;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.Data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return $"{sb.ToString()}";
+        }
+    }
+}
---
+++ Generics - Lab/1. Box of T/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            int input = 258;
+
+            box.Add(input);
+            box.Add(input);
+            box.Add(input);
+            box.Add(input);
+            box.Add(input);
+            //var result = box.Remove();
+
+            Console.WriteLine(box);
+        }
+    }
+}
