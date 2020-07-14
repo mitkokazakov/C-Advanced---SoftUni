3a36d05cee04158efb3dd854a53e452aec25b20d
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-14 19:47:28 +0300

    New task

---
+++ Generics - Exercise/1. Generic Box of String/StartUp.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GenericBoxF
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Box<string> box = new Box<string>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
