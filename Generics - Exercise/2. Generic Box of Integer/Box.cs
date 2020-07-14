101155e12742dfcb660efee9b9ede1f5ecb82fe7
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-14 19:44:26 +0300

    New task

---
+++ Generics - Exercise/2. Generic Box of Integer/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBox
+{
+    public class Box<T>
+    {
+        public Box(T item)
+        {
+            this.Value = item;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ Generics - Exercise/2. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using GenericBox;
+using System;
+using System.Collections.Generic;
+
+namespace GenereicBox
+{
+     class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Box<int>> boxes = new List<Box<int>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
