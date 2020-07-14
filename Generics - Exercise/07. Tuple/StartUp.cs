52d2a3fb5eea9e02327b04df9c21c37b8caa5de9
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-14 19:44:59 +0300

    New task has been added

---
+++ Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace TupleGeneric
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInputInfo = Console.ReadLine().Split();
+
+            string name = $"{firstInputInfo[0]} {firstInputInfo[1]}";
+            string adress = firstInputInfo[2];
+
+            string[] secondInputInfo = Console.ReadLine().Split();
+
+            string name1 = secondInputInfo[0];
+            int liters = int.Parse(secondInputInfo[1]);
+
+            string[] thirdInputInfo = Console.ReadLine().Split();
+
+            int n = int.Parse(thirdInputInfo[0]);
+            double d = double.Parse(thirdInputInfo[1]);
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(name,adress);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name1, liters);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(n, d);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
---
+++ Generics - Exercise/07. Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleGeneric
+{
+    public class Tuple<F,S>
+    {
+        public F FirstItem { get; set; }
+
+        public S SecondItem { get; set; }
+
+        public Tuple(F firstItem, S secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem}";
+        }
+    }
+}
