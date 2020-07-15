4ce0534f061a84b187f0d39b514f46907c24a2fa
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-15 23:30:57 +0300

    New task

---
+++ Generics - Exercise/Generic Swap Method Strings/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwap
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentLine = Console.ReadLine();
+                names.Add(currentLine);
+                
+            }
+
+            Swap<string> swap = new Swap<string>(names);
+
+            int[] indexes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            swap.SwapItems(swap.Values, indexes[0], indexes[1]);
+
+            Console.WriteLine(swap);
+        }
+    }
+}
---
+++ Generics - Exercise/Generic Swap Method Strings/Swap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwap
+{
+    class Swap<T>
+    {
+        public List<T> Values  { get; set; }
+
+        public Swap(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        public void SwapItems(List<T> names, int firstIndex, int secondindex)
+        {
+            T tempValue = names[firstIndex];
+            names[firstIndex] = names[secondindex];
+            names[secondindex] = tempValue;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var value in this.Values)
+            {
+                sb.AppendLine($"{value.GetType()}: {value.ToString()}");
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
