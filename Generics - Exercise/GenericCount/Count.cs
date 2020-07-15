ea8d8c3350fa04f783b64edfa54507d5977c000f
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-15 23:31:51 +0300

    Last task from this exercise

---
+++ Generics - Exercise/GenericCount/Count.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCount
+{
+    public class Count<T> where T : IComparable
+    {
+        public List<T> Items { get; set; }
+
+        public Count(List<T> items)
+        {
+            this.Items = items;
+        }
+
+        public int CountItems(List<T> values, T elementToCompare)
+        {
+            int count = 0;
+
+            foreach (var value in values)
+            {
+                int status = value.CompareTo(elementToCompare);
+
+                if (status == 1)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Generics - Exercise/GenericCount/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCount
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                items.Add(line);
+            }
+
+            Count<string> count = new Count<string>(items);
+
+            string elementToCompare = Console.ReadLine();
+
+            int countOfGreaterElements = count.CountItems(count.Items, elementToCompare);
+
+            Console.WriteLine(countOfGreaterElements);
+        }
+    }
+}
