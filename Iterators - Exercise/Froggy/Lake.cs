567f647fb12ff8b4246235a92681ec40ece5752a
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-18 16:27:39 +0300

    New task

---
+++ Iterators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public List<int> Names { get; set; }
+
+        public Lake(List<int> names)
+        {
+            this.Names = names;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.Names.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return this.Names[i];
+                }
+            }
+
+            for (int i = this.Names.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return this.Names[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators - Exercise/Froggy/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> names = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Lake lake = new Lake(names);
+
+            Console.WriteLine(String.Join(", ",lake));
+        }
+    }
+}
