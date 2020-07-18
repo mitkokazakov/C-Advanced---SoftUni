7899eff7d796c703b06da8e6e893e62dd274b3fa
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-18 16:25:56 +0300

    New task has been added

---
+++ Iterators - Exercise/Comparing Objects/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] personInfo = input.Split(" ");
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                string town = personInfo[2];
+
+                Person person = new Person(name,age,town);
+
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            Person personToCompare = people[n - 1];
+
+            int matches = 0;
+
+            foreach (var person in people)
+            {
+                if (personToCompare.CompareTo(person) == 0)
+                {
+                    matches++;
+                }
+            }
+
+
+            if (matches <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matches} {people.Count - matches} {people.Count}");
+            }
+            
+        }
+    }
+}
