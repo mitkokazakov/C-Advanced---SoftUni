f95c9472c6feb468569d4f042631f885716e0219
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-19 10:19:01 +0300

    Last task

---
+++ Iterators - Exercise/Stack/StackData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class StackData<T> : IEnumerable<T>
+    {
+        public List<T> AllElements;
+
+        public StackData()
+        {
+            this.AllElements = new List<T>();
+        }
+
+        public void Push(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                this.AllElements.Add(arr[i]);
+            }
+        }
+        public void Pop()
+        {
+            if (this.AllElements.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            else
+            {
+                this.AllElements.RemoveAt(this.AllElements.Count-1);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            
+                for (int i = this.AllElements.Count - 1; i >= 0; i--)
+                {
+                    yield return this.AllElements[i];
+                }
+
+                for (int i = this.AllElements.Count - 1; i >= 0; i--)
+                {
+                    yield return this.AllElements[i];
+                }
+            
+            
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators - Exercise/Stack/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string commands = Console.ReadLine();
+
+            StackData<string> stack = new StackData<string>();
+
+            while (commands != "END")
+            {
+                if (commands.Contains("Push"))
+                {
+                    string[] inputData = commands.Split(" ", 2);
+                    string[] elements = inputData[1].Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    stack.Push(elements);
+                }
+                else if (commands == "Pop")
+                {
+
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, stack));
+
+
+
+
+        }
+    }
+}
