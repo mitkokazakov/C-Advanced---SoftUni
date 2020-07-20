d962d6a677f4f9d9335d9951819b1e1958fecdb2
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-20 18:08:31 +0300

    New task has been added

---
+++ Defining Classes - Exercise/Raw Data/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> listCars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split();
+                string model = carInfo[0];
+
+                int engineSpeed = int.Parse(carInfo[1]);
+                int enginePower = int.Parse(carInfo[2]);
+
+                int cargoWeight = int.Parse(carInfo[3]);
+                string cargoType = carInfo[4];
+
+                double firstTirePressure = double.Parse(carInfo[5]);
+                int firstTireAge = int.Parse(carInfo[6]);
+
+                double secondTirePressure = double.Parse(carInfo[7]);
+                int secondTireAge = int.Parse(carInfo[8]);
+
+                double thirdTirePressure = double.Parse(carInfo[9]);
+                int thirdTireAge = int.Parse(carInfo[10]);
+
+                double fourthTirePressure = double.Parse(carInfo[11]);
+                int fourthTireAge = int.Parse(carInfo[12]);
+
+                Engine engine = new Engine(engineSpeed, enginePower);
+                Cargo cargo = new Cargo(cargoWeight, cargoType);
+                List<Tire> tires = new List<Tire>();
+                tires.Add(new Tire(firstTirePressure, firstTireAge));
+                tires.Add(new Tire(secondTirePressure, secondTireAge));
+                tires.Add(new Tire(thirdTirePressure, thirdTireAge));
+                tires.Add(new Tire(fourthTirePressure, fourthTireAge));
+
+                Car car = new Car(model,engine,cargo,tires);
+
+                listCars.Add(car);
+            }
+
+            string filter = Console.ReadLine();
+
+            if (filter == "fragile")
+            {
+                var result = listCars.Where(x => x.Cargo.Type == filter && x.Tires.Any(t => t.Pressure < 1));
+                Console.WriteLine(String.Join(Environment.NewLine,result));
+            }
+            else if (filter == "flamable")
+            {
+                var result = listCars.Where(x => x.Cargo.Type == filter && x.Engine.Power > 250);
+                Console.WriteLine(String.Join(Environment.NewLine, result));
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/Raw Data/Tire.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace RawData
+{
+    public class Tire
+    {
+        private double pressure;
+        private int age;
+
+        public Tire(double pressure, int age)
+        {
+            this.Pressure = pressure;
+            this.Age = age;
+        }
+
+        public double Pressure
+        {
+            get
+            {
+                return this.pressure;
+            }
+            set
+            {
+                this.pressure = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
