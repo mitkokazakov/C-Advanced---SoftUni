b1a5521196506be48dd79ffc1fcfaae041e2abaa
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-22 20:02:10 +0300

    New task

---
+++ Defining Classes - Exercise/Oldest Family Member/Family.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OldestFamilyMember
+
+{
+    public class Family
+    {
+        public List<Person> Members { get; set; }
+
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            this.Members.Add(person);
+        }
+
+        public Person GetOldestPerson()
+        {
+            Person oldestMember = this.Members.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return oldestMember;
+            
+        }
+    }
+}
---
+++ Defining Classes - Exercise/Oldest Family Member/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OldestFamilyMember
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int numOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person person = new Person(name, age);
+                
+                family.AddMember(person);
+            }
+
+            
+            Console.WriteLine(family.GetOldestPerson());
+        }
+    }
+}
